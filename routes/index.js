b2f0650a3f6c1e8d5d8cbb1dc091e1f23d21bf2e
GenaroMazur <genaromazur03@gmail.com>
2022-06-19 18:03:47 -0300

    [funcional]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs=require("fs");
+var path=require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+  let usersJSON=fs.readFileSync(path.join(__dirname,"./../models/users.JSON"),"utf-8")
+  let usersList=JSON.parse(usersJSON)
+  res.render('index', { title: 'Express',usersList:usersList});
 });
 
 module.exports = router;
